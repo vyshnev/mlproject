6d84a8d8f9b4232a1a207d4e9c2586248c76da52
Vyshnev <vyshnevnandanam@gmail.com>
2025-03-14 10:47:22 +0530

    Data ingestion

--- src/compontents/data_ingestion.py
+++ src/compontents/data_ingestion.py
@@ -29,7 +29,7 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info('train test split initiated')
 
-            train_set, test_set = train_test_split(df, train_size=0.2, random_state=42)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
